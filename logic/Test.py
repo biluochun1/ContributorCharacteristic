8292cadcfb0d77bceeb1ad0947ebf47e2563cc55
diowei <diowei@tencent.com>
2020-03-29 17:30:33 +0800

    ADD:增加一些代码逻辑

--- logic/Test.py
+++ logic/Test.py
@@ -1,9 +1,6 @@
 from github import github
 import json
 
-username = "823510561@qq.com"
-password = "823510561qq"
-
 
 def str2json(s):
     return json.loads(s.decode("utf-8"), encoding="utf-8")
@@ -18,7 +15,6 @@ def run():
     # print(resp)
 
 
-
 # def parse_4days_log():
 #     with open("/Users/weizijian/Downloads/gray_0305_data.txt", "w+") as fw:
 #         with open("/Users/weizijian/Downloads/gray_3days_full.txt", "r") as f:
@@ -29,12 +25,13 @@ def run():
 #                 vid = s[3].split("|")[1]
 #                 l = data + "\t" + vid
 #                 fw.write(l)
-                # print(vid)
+# print(vid)
 
 
 def parse_gitlog_json(path="/Users/weizijian/Downloads/bi_she_repo_download/grpc_gitlog.json"):
     log = json.load(open(path))
     print(log[0])
+
 
 if __name__ == '__main__':
     run()
